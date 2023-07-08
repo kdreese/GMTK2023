af5e671703813cb9d2a6ad26d6a052442ad072a2
Terry Hearst <thearst3rd@gmail.com>
2023-07-07 20:39:05 -0400

    Update project settings and add credits screen

--- src/states/menu/menu.gd
+++ src/states/menu/menu.gd
@@ -1,10 +1,17 @@
 extends Control
 
+
 func play() -> void:
 	pass
+
 
 func options() -> void:
 	pass
 
+
 func credits() -> void:
-	pass
+	get_tree().change_scene_to_file("res://src/states/menu/credits_menu.tscn")
+
+
+func quit() -> void:
+	get_tree().quit()
