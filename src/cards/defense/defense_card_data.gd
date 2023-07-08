66a72e2cb92733b07b8fcc26dccc03573bcdfbce
jokertyf <admoore@wpi.edu>
2023-07-08 16:31:30 -0400

    Added moving and attacking.

--- src/cards/defense/defense_card_data.gd
+++
@@ -1,9 +0,0 @@
-class_name DefenseCardData
-extends Resource
-
-
-@export var name: String ## The name of the card.
-@export_multiline var description: String ## A detailed description
-@export var icon: Texture2D ## The icon for this card.
-@export var rank: int ## The power rank of this card.
-@export_file() var effect ## A path to the effect this card will do.
--- src/cards/dual_card_data.gd
+++ src/cards/dual_card_data.gd
@@ -2,14 +2,18 @@ class_name DualCardData
 extends Resource
 
 
-@export var attack: AttackCardData
-@export var defense: DefenseCardData
+@export var attack: CardData
+@export var defense: CardData
 @export var rank: int
 
 
-func _init(att_data: AttackCardData, def_data: DefenseCardData) -> void:
+func _init(att_data: CardData, def_data: CardData) -> void:
 	if att_data.rank != def_data.rank:
 		push_warning("Ranks do not match.")
+	if att_data.card_role != "Attack":
+		push_warning("Attack card does not have attack role.")
+	if def_data.card_role != "Defense":
+		push_warning("Defense card does not have defense role.")
 	attack = att_data
 	defense = def_data
 	rank = att_data.rank
