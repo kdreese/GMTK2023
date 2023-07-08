1312407d04810e90f0c2c7956f09508e91cfa37f
jokertyf <admoore@wpi.edu>
2023-07-07 20:37:20 -0400

    Added card scene.

---
+++ src/cards/card.gd
@@ -0,0 +1,11 @@
+extends Control
+
+@onready var card_name: Label = %Name
+@onready var icon: TextureRect = %Icon
+@onready var description: Label = %Description
+
+
+func initialize(data: AttackCardData) -> void:
+	card_name.text = data.name
+	icon.texture = data.icon
+	description.text = data.description
