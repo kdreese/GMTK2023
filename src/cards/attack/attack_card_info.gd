c63725b56a39e4bcb7ce2593b0986d3dcf7afebf
kdreese <reesekyle1998@gmail.com>
2023-07-09 00:09:09 -0400

    Brought attack and defense cards in line with dual cards with icons

--- src/cards/attack/attack_card_info.gd
+++ src/cards/attack/attack_card_info.gd
@@ -12,3 +12,29 @@ func initialize(data: CardData) -> void:
 	icon.texture = data.icon
 	description.text = data.description
 	rank_icon.texture = Util.rank_to_texture(data.rank)
+	update_icons(data, $Stats)
+
+
+func update_icons(data: CardData, grid: GridContainer) -> void:
+	for node in grid.get_children():
+		node.hide()
+	if data is MeleeUnitData:
+		grid.get_node("HealthLabel").show()
+		grid.get_node("HealthLabel").text = str(data.health)
+		grid.get_node("HealthIcon").show()
+		grid.get_node("AttLabel").show()
+		grid.get_node("AttLabel").text = str(data.attack_power)
+		grid.get_node("AttIcon").show()
+		grid.get_node("MvmtLabel").show()
+		grid.get_node("MvmtLabel").text = str(data.speed)
+		grid.get_node("MvmtIcon").show()
+	elif data is RangedUnitData:
+		grid.get_node("AttLabel").show()
+		grid.get_node("AttLabel").text = str(data.attack_damage)
+		grid.get_node("AttIcon").show()
+		grid.get_node("RangeLabel").show()
+		grid.get_node("RangeLabel").text = str(data.attack_range)
+		grid.get_node("RangeIcon").show()
+	else:
+		grid.get_node("SpecialLabel").show()
+		grid.get_node("SpecialIcon").show()
