f6f911317d605823bbb635b652d1d105f33292ea
jokertyf <admoore@wpi.edu>
2023-10-04 21:09:09 -0400

    Text fitting.

--- src/util.gd
+++ src/util.gd
@@ -13,3 +13,11 @@ static func rank_to_texture(rank: int) -> Texture2D:
 	else:
 		push_error("Invalid rank %d" % rank)
 		return
+
+
+static func fit_text(label: Label, max_font_size: int = 16):
+	var font_size = max_font_size
+	var label_font = label.get_theme_font("font_size")
+	while label_font.get_string_size(label.text, label.horizontal_alignment, -1, font_size).x > label.size.x:
+		font_size -= 1
+	label.add_theme_font_size_override("font_size", font_size)
