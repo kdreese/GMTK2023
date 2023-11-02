50cd99c12363b5111dfce5610d5cf68345c2515a
kdreese <reesekyle1998@gmail.com>
2023-11-01 23:57:53 -0400

    Added viewer to tooltip for temporary stat changes

--- src/ui/tooltip.gd
+++ src/ui/tooltip.gd
@@ -13,16 +13,16 @@ const vert_offset = 42.0
 
 func initialize(unit: Unit) -> void:
 	if unit is RangedUnit:
-		damage_amt.text = str(unit.attack_damage)
+		damage_amt.text = str(unit.attack_damage) + add_extra_stats_text(unit, "attack_damage")
 		health_icon.hide()
 		health_amt.hide()
 		move_icon.hide()
 		move_amt.hide()
 
 	elif unit is MeleeUnit:
-		damage_amt.text = str(unit.attack_power)
-		health_amt.text = str(unit.health)
-		move_amt.text = str(unit.speed)
+		damage_amt.text = str(unit.attack_power) + add_extra_stats_text(unit, "attack_power")
+		health_amt.text = str(unit.health) + add_extra_stats_text(unit, "health")
+		move_amt.text = str(unit.speed) + add_extra_stats_text(unit, "speed")
 
 	if not unit.special_effect == "":
 		info_text.text = unit.special_effect
@@ -32,6 +32,12 @@ func initialize(unit: Unit) -> void:
 	call_deferred("set_pos", unit)
 
 
+func add_extra_stats_text(unit: Unit, stat_name: String) -> String:
+	if(unit.extra_stats.get(stat_name, 0)):
+		return "(%+d)" % int(unit.extra_stats.get(stat_name))
+	return ""
+
+
 func set_pos(unit: Unit) -> void:
 	var position_offset := Vector2(-100, -100)
 	if unit.grid_position.y < 3:
@@ -39,4 +45,5 @@ func set_pos(unit: Unit) -> void:
 	else:
 		position_offset.y -= vert_offset - 5
 	position = unit.position + position_offset
-	position.x = clampf(position.x, -30, 670)
+	position.x = clampf(position.x, -100 + size.x / 2, 740 - size.x / 2)
+	#position.x = clampf(position.x, -30, 670)
