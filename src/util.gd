07dec7ef037aed6cbbc98d47faba71f0b8d7bef1
jokertyf <admoore@wpi.edu>
2023-11-15 19:46:46 -0500

    Reworked rank icons.

--- src/util.gd
+++ src/util.gd
@@ -8,8 +8,6 @@ static func rank_to_texture(rank: int) -> Texture2D:
 		return preload("res://assets/ranks/rank_2.png")
 	elif rank == 3:
 		return preload("res://assets/ranks/rank_3.png")
-	elif rank == 4:
-		return preload("res://assets/ranks/rank_4.png")
 	else:
 		push_error("Invalid rank %d" % rank)
 		return
