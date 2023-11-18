8822c875cbac85ee0b79aaadcd390077bdec4d61
jokertyf <admoore@wpi.edu>
2023-11-18 15:09:23 -0500

    Simplified trebuchet.

--- src/cards/actions/trebuchet.gd
+++ src/cards/actions/trebuchet.gd
@@ -2,17 +2,15 @@ extends CardAction
 
 
 func get_squares(grid_pos: Vector2i) -> Array[Vector2i]:
-	var squares: Array[Vector2i] = [grid_pos, grid_pos + Vector2i(-1, 0)]
-	if grid_pos.y in [2, 5]:
-		squares += [grid_pos + Vector2i(0, -1), grid_pos + Vector2i(-1, -1)]
-	else:
-		squares += [grid_pos + Vector2i(0, 1), grid_pos + Vector2i(-1, 1)]
-	return squares
+	return [
+		grid_pos, grid_pos + Vector2i(-1, 0),
+		grid_pos + Vector2i(0, 1), grid_pos + Vector2i(-1, 1)
+	]
 
 
 ## An optional function which can prevent an action from being run (and card consumed).
 func can_perform(grid_pos: Vector2i, is_enemy: bool) -> bool:
-	return in_defending_area(grid_pos, is_enemy) and grid_pos.x != 1
+	return in_defending_area(grid_pos, is_enemy) and grid_pos.x != 1 and grid_pos.y not in [2, 5]
 
 
 ## The action which is performed when the card is dropped. Accepts the card data and position.
@@ -41,13 +39,3 @@ func perform_action(grid_pos: Vector2i, _is_enemy: bool) -> void:
 func negative_effects(grid_pos: Vector2i) -> Array[Vector2i]:
 	return get_squares(grid_pos)
 
-
-## A list of squares which show up as "hovering" if a card were to be placed here.
-func hovering_tiles(grid_pos: Vector2i) -> Array[Vector2i]:
-	if grid_pos.y == 3:
-		return [grid_pos]
-	elif grid_pos.y == 4:
-		return [grid_pos, grid_pos + Vector2i(0, 1)]
-	elif grid_pos.y == 5:
-		return [grid_pos, grid_pos + Vector2i(0, -1)]
-	return []
